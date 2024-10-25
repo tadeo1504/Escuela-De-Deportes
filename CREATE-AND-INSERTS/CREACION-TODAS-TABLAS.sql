91cb439ba1476fffc622af0ad82bc818ed072c9d
Tadeo1504 <tadeo1504r@.com>
2024-10-25 20:17:38 -0300

    creates and inserts

---
+++ CREATE-AND-INSERTS/CREACION-TODAS-TABLAS.sql
@@ -0,0 +1,49 @@
+create table actividades(
+id int primary key,
+descripcion varchar(50),
+costo int
+);
+
+CREATE TABLE login (
+correo varchar(20) primary key,
+contraseña varchar(20),
+);
+
+create table instructores(
+ci int primary key,
+nombre varchar(15),
+apellido varchar(15)
+);
+
+create table turnos(
+id int primary key,
+hora_inicio time,
+hora_final time
+);
+
+create table alumnos(
+ci int primary key,
+nombre varchar(15),
+apellido varchar(15),
+fecha_naciemiento date
+);
+
+create table clase(
+ci_instructor int,
+id_actividad int,
+id_turno int,
+dictada date,
+id int primary key,
+foreign key (id_actividad) references actividades(id),
+foreign key (ci_instructor) references instructores(ci),
+foreign key (id_turno) references turnos(id)
+);
+
+create table alumno_clase(
+id_clase int,
+ci_alumno int,
+id_equipamiento int,
+foreign key (id_clase) references clase(id),
+foreign key (ci_alumno) references alumnos(ci),
+foreign key (id_equipamiento) references equipamiento(id)
+);
