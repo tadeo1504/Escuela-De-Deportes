f14a10e216ed97fbf5f2493ed05a4b2f15ec844b
alfonsodanielrodriguez <alfonso.rodriguez@correo.ucu.edu.uy>
2024-10-24 23:10:33 -0300

    Avance v1.1

--- creacion-de-tabla/Obligatorio.py
+++ creacion-de-tabla/Obligatorio.py
@@ -3,39 +3,37 @@ import pyodbc
 
 class EscuelaDeportesNieve:
 
-    def submenuReportes():
-        print("1 Reporte de Instructores")
-        print("2 Reporte de Turnos")
-        print("3 Reporte de Alumnos")
-        print("4 Reporte de Actividades")
-        while opcion != 5:
-            if opcion == '1':
-                        """"""
-            elif opcion == '2':
-                """"""
-            elif opcion == '3':
-                """"""
-            elif opcion == '4':
-                """"""
-            elif opcion == '5':
-                print("Salir")
-                break
-            else:
-                print("Opción no válida. Intente nuevamente.")
+    def conectar_bd():
+        try:
+            conexion = pyodbc.connect(
+                'DRIVER={ODBC Driver 17 for SQL Server};'
+                'SERVER=DESKTOP-8L3U16R;'
+                'DATABASE=ObligatorioBD;'
+                'Trusted_Connection=yes;'
+            )
+            return conexion
+        except pyodbc.Error as e:
+            print("Error al conectar con la base de datos:", e)
 
-    def submenuModificaciones():
-        print("1 Modificación de Deportes")
-        print("2 Modificación de Horarios")
-        while opcion != 3:
-            if opcion == '1':
-                """"""
-            elif opcion == '2':
-                """"""
-            elif opcion == '3':
-                print("Salir")
-                break
-            else:
-                print("Opción no válida. Intente nuevamente.")
+def submenuReportes():
+    print("1 Reporte de Instructores")
+    print("2 Reporte de Turnos")
+    print("3 Reporte de Alumnos")
+    print("4 Reporte de Actividades")
+    while opcion != 5:
+        if opcion == '1':
+                    """"""
+        elif opcion == '2':
+            """"""
+        elif opcion == '3':
+            """"""
+        elif opcion == '4':
+            """"""
+        elif opcion == '5':
+            print("Salir")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
 def altaInstructor():
     print("Ingrese CI:")
@@ -45,7 +43,7 @@ def altaInstructor():
     print("Ingrese el apellido:")
     apellido = input()  
     print("Instructor registrado con éxito.")
-     
+#-------------------------------------------------------------------------------------------
     # Conectar a la base de datos
     conexion = conectar_bd()
     cursor = conexion.cursor()
@@ -63,7 +61,7 @@ def altaInstructor():
     finally:
         cursor.close()
         conexion.close()
-
+#-------------------------------------------------------------------------------------------
 
 def bajaInstructor():
     print("Ingrese CI del instructor a dar de baja:")
@@ -89,7 +87,7 @@ def bajaInstructor():
     finally:
         cursor.close()
         conexion.close()
-
+#-------------------------------------------------------------------------------------------
 def modificarInstructor():
     print("Ingrese CI del instructor a modificar:")
     ci = input() 
@@ -101,23 +99,23 @@ def modificarInstructor():
     # ACA PUEDE MODIFICAR TODO O NADA
     
 
-    def submenuABMinstructores():
-        print("\n--- ABM de Instructores ---")
-        print("1 Alta de Instructores")
-        print("2 Baja de Instructores")
-        print("3 Modificaciones de Instructores")
-        while opcion != 4:
-            if opcion == '1':
-                altaInstructor()
-            elif opcion == '2':
-                bajaInstructor()
-            elif opcion == '3':
-                modificarInstructor()
-            elif opcion == '4':
-                print("Salir")
-                break
-            else:
-                print("Opción no válida. Intente nuevamente.")
+def submenuABMinstructores():
+    print("\n--- ABM de Instructores ---")
+    print("1 Alta de Instructores")
+    print("2 Baja de Instructores")
+    print("3 Modificaciones de Instructores")
+    while opcion != 4:
+        if opcion == '1':
+            altaInstructor()
+        elif opcion == '2':
+            bajaInstructor()
+        elif opcion == '3':
+            modificarInstructor()
+        elif opcion == '4':
+            print("Salir")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
 def altaAlumno():
     print("Ingrese CI:")
@@ -147,112 +145,171 @@ def modificarAlumno():
     nuevo_apellido = input()  
     print("Datos del alumno actualizados con éxito.")
 
-    def submenuABMalumnos():
-        while True:
-            print("\n--- ABM de Alumnos ---")
-            print("1. Alta de alumnos")
-            print("2. Baja de alumnos")
-            print("3. Modificaciones de alumnos")
-            print("4. Salir")
-            opcion = input("Seleccione una opción: ")
-            
-            if opcion == '1':
-                altaAlumno()
-            elif opcion == '2':
-                bajaAlumno()
-            elif opcion == '3':
-                modificarAlumno()
-            elif opcion == '4':
-                print("Salir")
-                break
-            else:
-                print("Opción no válida. Intente nuevamente.")
-
-
-    def submenuABMturnos():
-        print("\n--- ABM de Turnos ---")
-        print("1 Alta de turnos")
-        print("2 Baja de turnos")
-        print("3 Modificaciones de turnos")
-        while opcion != 5:
-            if opcion == '1':
-                        """"""
-            elif opcion == '2':
-                """"""
-            elif opcion == '3':
-                """"""
-            elif opcion == '4':
-                print("Salir")
-                break
-            else:
-                print("Opción no válida. Intente nuevamente.")
+def submenuABMalumnos():
+    while True:
+        print("\n--- ABM de Alumnos ---")
+        print("1. Alta de alumnos")
+        print("2. Baja de alumnos")
+        print("3. Modificaciones de alumnos")
+        print("4. Salir")
+        opcion = input("Seleccione una opción: ")
         
-    def costosDeportes():
-        """"""
+        if opcion == '1':
+            altaAlumno()
+        elif opcion == '2':
+            bajaAlumno()
+        elif opcion == '3':
+            modificarAlumno()
+        elif opcion == '4':
+            print("Salir")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+def asignarInstructores():
+    print("Ingrese el deporte:")
+    deporte = input()
+    print("Ingrese el instructor:")
+    instructor = input()
+    print("Instructor asignado con éxito.")
+
+def asignarAlumnos():
+    print("Ingrese el deporte:")
+    deporte = input()
+    print("Ingrese el alumno:")
+    alumno = input()
+    print("Alumno asignado con éxito.")
+
+def verActividades():
+    query = "SELECT * FROM actividades;"
+    cursor.execute(query)
+
+
+def submenuGestionClases():
+    print("1 Asignar instructores a actividades")
+    print("2 Asignar alumnos a actividades")
+    print("3 Ver actividades")
+    while opcion != 4:
+        if opcion == '1':
+            asignarInstructores()
+        elif opcion == '2':
+            asignarAlumnos()
+        elif opcion == '3':
+            verActividades()
+        elif opcion == '4':
+            print("Salir")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+def modificacionHorarios():
+    print("Ingrese el horario a modificar: ")
+    horario = input()
+    print("Ingrese el nuevo horario: ")
+    nuevo_horario = input()
+    print("Horario modificado con éxito.")  
+
+def modificarDeportes():
+    print("Ingrese el deporte a modificar: ")
+    deporte = input()
+    print("Ingrese el nuevo deporte: ")
+    nuevo_deporte = input()
+    print("Deporte modificado con éxito.")
     
-    def restriccionesDeportes():
-        """"""
+def submenuModificacionesActividades():
+    print("1 Modificación de Deportes")
+    print("2 Modificación de Horarios")
+    while opcion != 3:
+        if opcion == '1':
+            modificarDeportes()
+        elif opcion == '2':
+            modificacionHorarios()
+        elif opcion == '3':
+            print("Salir")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
-    def verDeportes():
-        query = "SELECT * FROM actividades;"
-        cursor.execute(query)
+def altaTurnos():
+    print("Ingrese el horario:")
+    horario = input()
+    print("Ingrese la fecha (DD/MM/AAAA):")
+    fecha = input()
+    print("Turno registrado con éxito.")
 
-    def menu():
-            while True:
-                print("\n--- Menú ---\n--- Deportes de Nieve ---")
-                print("1. ABM de Instructores")    #Alta, baja y modificación (ABM)
-                print("2. ABM de Turnos")
-                print("3. ABM de Alumnos")
-                print("4. Modificación de Actividades")
-                print("5. Gestión de Clases")
-                print("6. Reportes")
-                print("7. Salir")
-                opcion = input("Seleccione una opción: ")
+def bajaTurnos():
+    print("Ingrese el horario del turno a dar de baja:")
+    horario = input()
+    print("Ingrese la fecha del turno a dar de baja:")
+    fecha = input()
+    print("Turno dado de baja con éxito.")
 
-                if opcion == '1':
-                    """"""
-                elif opcion == '2':
-                    """"""
-                elif opcion == '3':
-                    """"""
-                elif opcion == '4':
-                    """"""
-                elif opcion == '5':
-                    """"""
-                elif opcion == '6':
-                    """"""
-                elif opcion == '7':
-                    print("Salir")
-                    break
-                else:
-                    print("Opción no válida. Intente nuevamente.")
+def modificarTurnos():
+    print("Ingrese el horario del turno a modificar:")
+    horario = input()
+    print("Ingrese la fecha del turno a modificar:")
+    fecha = input()
+    print("Ingrese el nuevo horario (dejar vacío si no desea cambiar):")
+    nuevo_horario = input()
+    print("Ingrese la nueva fecha (dejar vacío si no desea cambiar):")
+    nueva_fecha = input()
+    print("Datos del turno actualizados con éxito.")
 
-    def Saludo():
-        print("Hello juanita")
 
-    try:
-        # Configura la conexión con autenticación de Windows de SQL Server
-        conn = pyodbc.connect(
-                'DRIVER={ODBC Driver 17 for SQL Server};'
-                'SERVER=DESKTOP-8L3U16R;'
-                'DATABASE=ObligatorioBD;'
-                'Trusted_Connection=yes;'
-            )
-        cursor = conn.cursor()
+def submenuABMturnos():
+    print("\n--- ABM de Turnos ---")
+    print("1 Alta de turnos")
+    print("2 Baja de turnos")
+    print("3 Modificaciones de turnos")
+    while opcion != 5:
+        if opcion == '1':
+            altaTurnos()
+        elif opcion == '2':
+            bajaTurnos()
+        elif opcion == '3':
+            modificarTurnos()
+        elif opcion == '4':
+            print("Salir")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
-            # Ejecutar una consulta de ejemplo
-        query = "SELECT * FROM actividades;"
-        cursor.execute(query)
+def menu():
+        while True:
+            print("\n--- Menú ---\n--- Deportes de Nieve ---")
+            print("1. ABM de Instructores")    #Alta, baja y modificación (ABM)
+            print("2. ABM de Turnos")
+            print("3. ABM de Alumnos")
+            print("4. Modificación de Actividades")
+            print("5. Gestión de Clases")
+            print("6. Reportes")
+            print("7. Salir")
+            opcion = input("Seleccione una opción: ")
 
-            # Mostrar los resultados
-        for row in cursor:
-            print(row)
+            if opcion == '1':
+                submenuABMinstructores()
+            elif opcion == '2':
+                submenuABMturnos()
+            elif opcion == '3':
+                submenuABMalumnos()
+            elif opcion == '4':
+                submenuModificacionesActividades()
+            elif opcion == '5':
+                submenuGestionClases()
+            elif opcion == '6':
+                submenuReportes()
+            elif opcion == '7':
+                print("Salir")
+                break
+            else:
+                print("Opción no válida. Intente nuevamente.")
 
-        conn.close()
-        menu()
-        Saludo()
+def Saludo():
+    print("Hello juanita")
+
+if __name__ == '__main__':
+    menu()
+    Saludo()
+
         
-    except pyodbc.Error as e:
-        print("Error al conectar con la base de datos:", e)
-
-
+    
